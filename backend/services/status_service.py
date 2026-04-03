ee9a9db6d8d8715aa55644d734755ee4da933432
Заречнев Владимир <vzarecnev6@gmail.com>
2026-04-03 12:27:43 +0700

    Исправлена ошибка в файле status_service.py, снова.

--- backend/services/status_service.py
+++ backend/services/status_service.py
@@ -7,8 +7,8 @@ def get_status_data():
     }
 
 def get_items_data():
-    return {
+    return [
         {"id": 1, "name": "Товар 1"},
         {"id": 2, "name": "Товар 2"},
         {"id": 3, "name": "Товар 3"},
-    }
+    ]
