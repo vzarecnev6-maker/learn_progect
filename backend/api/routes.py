ac095464e45370b8ba78dfcaecc67cf6feda6483
Заречнев Владимир <vzarecnev6@gmail.com>
2026-04-20 07:41:39 +0300

    Исправлена ошибка в файле routes.py

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -1,6 +1,6 @@
 from backend.services.status_service import  get_status_data, get_items_data
 from backend.schemas.status_schema import StatusOutSchema
-from backend.schemas.item_schema import ItemSchema
+from backend.schemas.item_schema import ItemsOutSchema
 
 def register_routes(app):
     @app.get("/api/status")
@@ -9,6 +9,6 @@ def register_routes(app):
         return get_status_data()
 
     @app.get("/api/items")
-    @app.output(ItemSchema)
+    @app.output(ItemsOutSchema)
     def api_items():
         return {"items": get_items_data()}
